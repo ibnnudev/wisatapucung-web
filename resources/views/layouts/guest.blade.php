a4e1fb72d61f4e3c3a5d2463738b14f8fedaf1a1
ibnnudevv <ibnudev@medeva.tech>
2024-11-06 19:50:00 +0700

    Add initial project structure with configuration files, components, and basic styling

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://rsms.me/">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+
+    <style>
+        * {
+            font-family: InterVariable, system-ui, sans-serif !important;
+            font-feature-settings: "cv02", "cv03", "cv04", "cv11";
+        }
+    </style>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 antialiased h-screen">
+    {{ $slot }}
+</body>
+
+</html>
