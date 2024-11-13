fa41a73bddd56edbb1b5874fff256f05fb4ed435
ibnnudevv <ibnudev@medeva.tech>
2024-11-13 23:45:30 +0700

    fix

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,29 +1,17 @@
 @props(['theme' => 'light'])
 
 <header class="absolute inset-x-0 top-0 z-50">
-<<<<<<< HEAD
     <nav class="flex items-center justify-between px-6 lg:px-8" aria-label="Global">
-=======
-    <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
->>>>>>> 603888b4 (deploy)
         <div class="flex lg:flex-1">
             <a href="#" class="-m-1.5 p-1.5">
                 <span class="sr-only">
                     Wisata Edukasi Kalipucung
                 </span>
-<<<<<<< HEAD
                 <img class="h-24 w-auto p-2" src="{{ asset('images/logo_atas.png') }}" alt="" loading="lazy">
             </a>
         </div>
         <div class="flex lg:hidden">
             <button type="button" id="mobile-menu-btn"
-=======
-                <img class="h-24 w-auto" src="{{ asset('images/logo_atas.png') }}" alt="" loading="lazy">
-            </a>
-        </div>
-        <div class="flex lg:hidden">
-            <button type="button"
->>>>>>> 603888b4 (deploy)
                 class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
                 <span class="sr-only">Open main menu</span>
                 <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
@@ -86,93 +74,91 @@
         </div>
     </nav>
     <!-- Mobile menu, show/hide based on menu open state. -->
-<<<<<<< HEAD
-    <div class="block lg:hidden">
+    <<<<<<< HEAD <div class="block lg:hidden">
         <div class="fixed inset-0 z-50 bg-gray-900 bg-opacity-50 hidden" id="mobile-menu-backdrop"></div>
         <div class="fixed inset-y-0 right-0 z-50 w-full max-w-sm overflow-y-auto bg-white px-6 py-6 sm:ring-1 sm:ring-gray-900/10 hidden"
             id="mobile-menu">
             <div class="flex items-center justify-between">
                 <span></span>
                 <button type="button" id="mobile-menu-close-btn" class="-m-2.5 rounded-md p-2.5 text-gray-700">
-=======
-    <div class="lg:hidden" role="dialog" aria-modal="true">
-        <!-- Background backdrop, show/hide based on slide-over state. -->
-        <div class="fixed inset-0 z-50"></div>
-        <div
-            class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div class="flex items-center justify-between">
-                <a href="#" class="-m-1.5 p-1.5">
-                    <span class="sr-only">Your Company</span>
-                    <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                        alt="">
-                </a>
-                <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
->>>>>>> 603888b4 (deploy)
-                    <span class="sr-only">Close menu</span>
-                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                        aria-hidden="true" data-slot="icon">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-            <div class="mt-6 flow-root">
-                <div class="-my-6 divide-y divide-gray-500/10">
-                    <div class="space-y-2 py-6">
-<<<<<<< HEAD
-                        <a href="/"
-                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Beranda</a>
-                        <a href="{{ route('user.about') }}"
-                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Tentang
-                            Kami</a>
-                        <a href="{{ route('user.demography') }}"
-                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Kondisi
-                            Geografis</a>
-                        <a href="{{ route('user.attraction') }}"
-                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Atraksi</a>
-                        {{-- informasi link --}}
-                        <a href="{{ route('user.information') }}"
-                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Informasi</a>
-                        <a href="{{ route('user.accommodation') }}"
-                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Penginapan</a>
-                        <a href="{{ route('user.tour-package') }}"
-                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Paket
-                            Wisata</a>
-                        {{-- end informasi link --}}
-                        <a href="{{ route('user.product') }}"
-                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Produk</a>
-                        <a href="{{ route('user.organization') }}"
-                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Organisasi</a>
-                        <a href="{{ route('user.contact') }}"
-                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Kontak
-                            Kami</a>
+                    =======
+                    <div class="lg:hidden" role="dialog" aria-modal="true">
+                        <!-- Background backdrop, show/hide based on slide-over state. -->
+                        <div class="fixed inset-0 z-50"></div>
+                        <div
+                            class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                            <div class="flex items-center justify-between">
+                                <a href="#" class="-m-1.5 p-1.5">
+                                    <span class="sr-only">Your Company</span>
+                                    <img class="h-8 w-auto"
+                                        src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                                        alt="">
+                                </a>
+                                <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+                                    >>>>>>> 603888b4 (deploy)
+                                    <span class="sr-only">Close menu</span>
+                                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                        stroke="currentColor" aria-hidden="true" data-slot="icon">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div class="mt-6 flow-root">
+                                <div class="-my-6 divide-y divide-gray-500/10">
+                                    <div class="space-y-2 py-6">
+                                        <<<<<<< HEAD <a href="/"
+                                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                                            Beranda</a>
+                                            <a href="{{ route('user.about') }}"
+                                                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Tentang
+                                                Kami</a>
+                                            <a href="{{ route('user.demography') }}"
+                                                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Kondisi
+                                                Geografis</a>
+                                            <a href="{{ route('user.attraction') }}"
+                                                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Atraksi</a>
+                                            {{-- informasi link --}}
+                                            <a href="{{ route('user.information') }}"
+                                                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Informasi</a>
+                                            <a href="{{ route('user.accommodation') }}"
+                                                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Penginapan</a>
+                                            <a href="{{ route('user.tour-package') }}"
+                                                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Paket
+                                                Wisata</a>
+                                            {{-- end informasi link --}}
+                                            <a href="{{ route('user.product') }}"
+                                                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Produk</a>
+                                            <a href="{{ route('user.organization') }}"
+                                                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Organisasi</a>
+                                            <a href="{{ route('user.contact') }}"
+                                                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Kontak
+                                                Kami</a>
+                                    </div>
+                                    <div class="py-6">
+                                        <a href="{{ route('login') }}"
+                                            class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Masuk</a>
+                                        =======
+                                        <a href="#"
+                                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Product</a>
+                                        <a href="#"
+                                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Features</a>
+                                        <a href="#"
+                                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Marketplace</a>
+                                        <a href="#"
+                                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Company</a>
+                                    </div>
+                                    <div class="py-6">
+                                        <a href="#"
+                                            class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log
+                                            in</a>
+                                        >>>>>>> 603888b4 (deploy)
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="py-6">
-                        <a href="{{ route('login') }}"
-                            class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Masuk</a>
-=======
-                        <a href="#"
-                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Product</a>
-                        <a href="#"
-                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Features</a>
-                        <a href="#"
-                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Marketplace</a>
-                        <a href="#"
-                            class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Company</a>
-                    </div>
-                    <div class="py-6">
-                        <a href="#"
-                            class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log
-                            in</a>
->>>>>>> 603888b4 (deploy)
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 </header>
-<<<<<<< HEAD
-
-<script>
+<<<<<<< HEAD <script>
     const mobileMenuBtn = document.getElementById('mobile-menu-btn');
     const mobileMenuBackdrop = document.getElementById('mobile-menu-backdrop');
     const mobileMenu = document.getElementById('mobile-menu');
