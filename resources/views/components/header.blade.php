35a5b9269631a68a076db35371f534dab3e5a5dd
ibnnudevv <ibnudev@medeva.tech>
2024-11-13 23:52:58 +0700

    Refactor header navigation layout for improved responsiveness

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,8 +1,8 @@
 @props(['theme' => 'light'])
 
 <header class="absolute inset-x-0 top-0 z-50">
-    <nav class="flex items-center justify-between px-6 lg:px-8" aria-label="Global">
-        <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <nav class="flex items-center justify-between" aria-label="Global">
+        <nav class="flex items-center justify-between p-6 lg:px-8 w-full" aria-label="Global">
             <div class="flex lg:flex-1">
                 <a href="#" class="-m-1.5 p-1.5">
                     <span class="sr-only">
