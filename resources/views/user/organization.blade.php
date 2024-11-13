cebb8bc8ca6f6851058c5c2750dff40a6de6628b
ibnnudevv <ibnudev@medeva.tech>
2024-11-13 23:54:09 +0700

    Increase padding in organization section for improved layout

--- resources/views/user/organization.blade.php
+++ resources/views/user/organization.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <div class="bg-gray-50">
         <x-header theme="dark" />
-        <section class="bg-[#FCF8F1] bg-opacity-30 py-32 lg:py-32">
+        <section class="bg-[#FCF8F1] bg-opacity-30 py-48">
             <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
                     <div>
